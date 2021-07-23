2b34b74c597f2a1210dbb87c710db7502f5f693b
YuSunjo <tnswh2023@gmail.com>
2021-07-24 06:30:22 +0900

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    head: {
+        title: 'nodeBird',
+    },
+    modules: [
+        '@nuxtjs/vuetify',
+        '@nuxtjs/axios'
+    ],
+    plugin: [
+
+    ],
+    vuetify: {
+
+    }
+}
