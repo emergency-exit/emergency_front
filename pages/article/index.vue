22ebc8948d77f1bba150649864615d43c653223c
zzoyu <zzoyu@hotmail.com>
2021-10-20 01:51:07 +0900

    asyncData 테스트

---
+++ pages/article/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div />
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  async asyncData( { params, $http } ) {
+    console.log(await axios.get("http://ec2-3-38-124-11.ap-northeast-2.compute.amazonaws.com/ping").data);
+  }
+
+}
+</script>
+
+<style>
+
+</style>
